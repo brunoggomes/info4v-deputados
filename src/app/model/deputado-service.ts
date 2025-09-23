b41b7b2c90e298497358b09a0db32c1fe7c9c7a7
Bruno Gomes <bruno_gurgel@yahoo.com.br>
2025-09-23 15:59:32 -0300

    HTTP client adicionado

--- src/app/model/deputado-service.ts
+++ src/app/model/deputado-service.ts
@@ -1,106 +1,24 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Deputado } from './deputado';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeputadoService {
-  private deputados: Deputado[] = [
-  {
-    id: "1",
-    nome: "Carlos Henrique da Silva",
-    email: "carlos.silva@camara.leg.br",
-    idLegislatura: "57",
-    siglaPartido: "PDS",
-    siglaUf: "SP",
-    urlFoto: "https://fotos.camara.leg.br/deputado1.jpg"
-  },
-  {
-    id: "2",
-    nome: "Fernanda Souza Lima",
-    email: "fernanda.lima@camara.leg.br",
-    idLegislatura: "57",
-    siglaPartido: "PTN",
-    siglaUf: "RJ",
-    urlFoto: "https://fotos.camara.leg.br/deputado2.jpg"
-  },
-  {
-    id: "3",
-    nome: "João Batista Rocha",
-    email: "joao.rocha@camara.leg.br",
-    idLegislatura: "57",
-    siglaPartido: "MDB",
-    siglaUf: "MG",
-    urlFoto: "https://fotos.camara.leg.br/deputado3.jpg"
-  },
-  {
-    id: "4",
-    nome: "Mariana Costa e Silva",
-    email: "mariana.costa@camara.leg.br",
-    idLegislatura: "57",
-    siglaPartido: "PVL",
-    siglaUf: "RS",
-    urlFoto: "https://fotos.camara.leg.br/deputado4.jpg"
-  },
-  {
-    id: "5",
-    nome: "Rafael Oliveira Mendes",
-    email: "rafael.mendes@camara.leg.br",
-    idLegislatura: "57",
-    siglaPartido: "PDT",
-    siglaUf: "BA",
-    urlFoto: "https://fotos.camara.leg.br/deputado5.jpg"
-  },
-  {
-    id: "6",
-    nome: "Luciana Torres Barreto",
-    email: "luciana.barreto@camara.leg.br",
-    idLegislatura: "57",
-    siglaPartido: "PSB",
-    siglaUf: "PE",
-    urlFoto: "https://fotos.camara.leg.br/deputado6.jpg"
-  },
-  {
-    id: "7",
-    nome: "Ricardo Fernandes Lopes",
-    email: "ricardo.lopes@camara.leg.br",
-    idLegislatura: "57",
-    siglaPartido: "PRL",
-    siglaUf: "GO",
-    urlFoto: "https://fotos.camara.leg.br/deputado7.jpg"
-  },
-  {
-    id: "8",
-    nome: "Ana Paula Martins",
-    email: "ana.martins@camara.leg.br",
-    idLegislatura: "57",
-    siglaPartido: "UNI",
-    siglaUf: "SC",
-    urlFoto: "https://fotos.camara.leg.br/deputado8.jpg"
-  },
-  {
-    id: "9",
-    nome: "Eduardo Nascimento Alves",
-    email: "eduardo.alves@camara.leg.br",
-    idLegislatura: "57",
-    siglaPartido: "PSD",
-    siglaUf: "CE",
-    urlFoto: "https://fotos.camara.leg.br/deputado9.jpg"
-  },
-  {
-    id: "10",
-    nome: "Tatiane Ribeiro Cunha",
-    email: "tatiane.cunha@camara.leg.br",
-    idLegislatura: "57",
-    siglaPartido: "REP",
-    siglaUf: "PA",
-    urlFoto: "https://fotos.camara.leg.br/deputado10.jpg"
-  }
-];
+  //inject(HttpClient) cria o objeto do serviço HttpClient
+  private http = inject(HttpClient)
+  private APIURL = 'https://dadosabertos.camara.leg.br/api/v2'
+  private deputados: Deputado[] = [];
 
 
   obterDeputados(): Deputado[] {
+    this.http.get(`${this.APIURL}/deputados?ordem=ASC&ordenarPor=nome`).subscribe(
+      res => {
+        console.log(res)
+      })
+      
     return [...this.deputados]
   }
 
